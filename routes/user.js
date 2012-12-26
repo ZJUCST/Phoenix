0fd49702e460c7c29f622e50e24934560bbc243f
Don_F <544213526@qq.com>
2012-12-26 19:24:16 +0800

    user.js

--- routes/user.js
+++ routes/user.js
@@ -2,43 +2,109 @@
 /*
  * GET users listing.
  */
-var user = require('../node_modules/dal/index').User;
+var user = require('dal/index').User;
+var crypto = require('crypto');
 
 exports.list = function(req, res){
-    /*res.send("respond with a resource");
-    console.log("user entroy point!!!!");
-    var log = {uname:req.body.name1,password:req.body.password1};
-    var reg = {uname:req.body.name2,password:req.body.password2};
-    if(log.uname ==""&&reg.uname =="")
+};
+
+exports.login=function(req,res){
+    console.log("entry login.js ");
+    var errString="";
+
+    var crypto = require('crypto');
+    //var salt = crypto.randomBytes(128).toString('base64');
+    //var password;
+    //crypto.pbkdf2( req.body.password, salt, 10000, 512, function(err, dk) { password = dk; } );
+
+    var log={
+        name:req.body.username,
+        password:password
+    };
+
+    if(log.name==""&&log.password=="")
     {
-        console.log("nothing is filled!");
-        res.redirect("");
+        console.log("nothing is filled ");
     }
-    else if(log.uname != "")
+    else if(log.name!=""&&log.password!="")
     {
-        user.find({name:log.uname,password:log.password},{name:1},function(err,doc){
+        user.find({name:log.name},{},function(err,docs){
             if(err)
             {
-                console.log("something is err!!!!")
+                console.log("something is wrong !!!");
             }
             else
             {
-                doc.toArray(function(err,items){
-                    if(items.length==0)
-                        console.log("the user dones't exist!");
+                docs.toArray(function(err,items){
+                    if(err)
+                        console.log("docs is error");
                     else
                     {
-                        console.log("OK");
+                        if(items.length==0)   //代表没有这个用户名
+                        {
+                            errString="The name dones't exist!!";
+                            res.send(errString);
+
+                        }
+                        else if(items.length==1)  //代表有这个用户名
+                        {
+                            var upassword ;
+                            var salt = items[0].salt;
+                            crypto.pbkdf2( req.body.password, salt, 10000, 512, function(err, dk) { upassword = dk; } );
+                            if(items[0].password == upassword) //代表密码正确
+                            {
+                                res.session.add(log);
+                                console.log("log suc");
+                            }
+                            else  //代表密码不正确
+                            {
+                                errString="the password is wrong";
+                                res.send(errString);
+                                console.log("the password is Wrong");
+                            }
+                            //res.session.add(log);
+                        }
                     }
                 })
             }
-        });
+        })
     }
-    else if(reg.uname !="")
-    {
-         user.insert({name:reg.name,password:reg.password},function(err,doc){
-             if(err)
-                console.log("when insert into table user ,something is wrong!");
-         })
-    } */
+};
+
+exports.reg = function(req,res){
+    console.log("entry reg point !!!!!!!");
+    var errString="";
+
+    var salt = crypto.randomBytes(128).toString('base64');
+    var password;
+    crypto.pbkdf2( req.body.password, salt, 10000, 512, function(err, dk) { password = dk; } );
+
+    var reg={
+        name:req.body.username,
+        password:password,
+        email:req.body.email,
+        salt:salt
+    };
+    user.find({name:reg.name},function(err,docs){
+        if(err)
+            console.log("something is wrong !");
+        else
+        {
+            docs.toArray(function(err,items){
+                if(items.length == 0)
+                {
+                    console.log("OK");
+                    user.insert(reg,function(err,docs){})
+                    res.session.add(reg);
+                }
+                else
+                {
+                    console.log("this user is already exists!");
+                    errString="server err";
+                    res.writeHead(200, {"Content-Type": "text/plain"});
+                    res.send(errString);
+                }
+            })
+        }
+    })
 };
