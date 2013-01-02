f55ac02d77d01af8b0171b3a6db4fbca984ea03b
zmzsmnh <zmzsmnh@gmail.com>
2013-01-02 14:33:39 +0800

    modify get image function to index page

--- test.js
+++ test.js
@@ -6,9 +6,18 @@
  * To change this template use File | Settings | File Templates.
  */
 
-var request = require('request');
+//var request = require('request');
+//
+//request.post('http://localhost:3000/collect/image', {form: {
+//    from: 'weibo.com',
+//    uri: 'http://www.upyun.com/images/uclund.png'
+//}}, function(err, res, body){ console.log(res.statusCode, body);});
 
-request.post('http://localhost:3000/collect/image', {form: {
-    from: 'weibo.com',
-    uri: 'http://www.upyun.com/images/uclund.png'
-}}, function(err, res, body){ console.log(res.statusCode, body);});
+var fs = require('fs');
+
+fs.readFile('/tmp/795497fbe69f6fcf12bab35b4e9ffc0f', function(err, data) {
+    console.log(err);
+    if(data) {
+    console.log(data);
+    }
+})
